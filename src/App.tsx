911de5a29d9243ecada716e93997ae540d54e634
Jonas Vignal-Olsen <jonasvignal@gmail.com>
2026-06-22 11:05:55 +0200

    logo changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ function App() {
   const buttons = [
     { name: 'Vikings 1', url: 'http://192.168.4.62:14999/start' },
     { name: 'Vikings 2', url: 'http://192.168.4.253:14999/start' },
-    { name: 'Generalen', url: 'http://192.168.4.104:14999/start' },
+    { name: 'Generalen', url: 'http://192.168.2.104:14999/start' },
     { name: 'Dr. Evil', url: 'http://192.168.4.28:14999/start' }
   ];
 
   return (
     <div className="app-container">
       <div className="logo-container">
-        <img src="/logo.png" alt="ESC Mission Control Logo" className="logo" />
+        <h1 className="logo-text">ESC MISSION CONTROL</h1>
       </div>
       <div className="buttons-container">
         {buttons.map((btn, index) => (
